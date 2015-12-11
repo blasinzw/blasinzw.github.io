14d17bf553df6e1f15caf9fc2825aa6b49e02533
blasinzw <zwblasingame@gmail.com>
2015-12-10 21:50:26 -0500

    Updated contact form.

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -23,7 +23,7 @@ if(isset($_POST['message'])){
 		$res['message'] = 'Form Submission Successful';
     }
 	else{
-		$res['message'] = 'Failed to send mail. Please mail me to blasinzw@clarkson.edu';
+		$res['message'] = 'Failed to send mail. Please mail me at blasinzw@clarkson.edu';
 	}
 
 
